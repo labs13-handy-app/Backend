21fa1190c0faa1ac618cd4ac85e073a14d1626b5
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-05-31 16:49:15 -0700

    Migrations done

---
+++ data/migrations/20190531124055_users_table.js
@@ -0,0 +1,40 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('users', tbl => {
+        
+        tbl
+        .increments();
+    
+         tbl
+        .string('name')
+        
+         tbl
+        .string('nickname')
+    
+        tbl
+        .string('account_type')
+
+        tbl
+        .string('email')
+
+        tbl
+        .string('address')
+
+        tbl
+        .string('phone_number')
+
+        tbl
+        .string('skills')
+
+        tbl
+        .string('experience')
+
+        tbl
+        .string('licenses')
+    
+    });
+};
+
+exports.down = function(knex, Promise) {
+return knex.schema.dropTableIfExists('users');
+};
