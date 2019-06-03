48efc468b23aa21e907b3bc474268e46d805aeb4
tjforeman <tylerforeman92@gmail.com>
2019-06-03 13:29:13 -0400

    setting up endpoint for register

--- auth-route/register.js
+++ auth-route/register.js
@@ -1,4 +1,32 @@
 const router = require('express').Router();
+const db =require('../data/dbConfig.js')
+const jwtDecode=require('jwt-decode')
+
+router.post('/', (req, res) => {
+
+  const token=req.headers.authorization
+
+  const decode=jwtDecode(token)
+
+  const user={
+    email:decode.email,
+    name:decode.name
+  }
+
+  db('users')
+  .where(user.email)
+  .then(res =>{
+    if(user.email){
+      res.status(200).json('You have logged in')
+    }else{
+      db('users')
+      .insert(user)
+    }
+  })
+  .catch(err=>{
+      res.send(err.message).json({message:'unable to sign up'})
+  })
+})
 
 
 module.exports=router
