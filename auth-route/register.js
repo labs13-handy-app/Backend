e178cfebb6a0ed4cf69875e6e738bb4b31ede6f5
tjforeman <tylerforeman92@gmail.com>
2019-06-03 14:58:57 -0400

    refactored register route, added register model

--- auth-route/register.js
+++ auth-route/register.js
@@ -1,32 +1,49 @@
 const router = require('express').Router();
-const db =require('../data/dbConfig.js')
 const jwtDecode=require('jwt-decode')
+const db = require('./register-model.js');
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try{
 
   const token=req.headers.authorization
 
-  const decode=jwtDecode(token)
+  if(req.headers && req.headers.authorization && token) {
+
+  const userToken = token.replace(/Bearer /g, '');
+
+  const decode=jwtDecode(userToken)
 
   const user={
     email:decode.email,
     name:decode.name
   }
 
-  db('users')
-  .where(user.email)
-  .then(res =>{
-    if(user.email){
-      res.status(200).json('You have logged in')
-    }else{
-      db('users')
-      .insert(user)
+  const foundUser = await db.getUserByName(name);
+  if (!foundUser) {
+    // if the user doesn't exist create a user object that reflect the database schema.
+    const newUser = {
+      name: name,
+      nickname,
+      email: email,
+    };
+  
+    const data = await db.addUser(newUser);
+        res.status(201).json(data);
+      } else {
+       
+        res.status(200).json(foundUser);
+      }
+    } else {
+  
+      res.status(400).json({errorMessage: 'Invalid Credentials!'});
     }
-  })
-  .catch(err=>{
+    
+    }catch(err){
       res.send(err.message).json({message:'unable to sign up'})
-  })
-})
+      
+    }
+    });
+
 
 
 module.exports=router
