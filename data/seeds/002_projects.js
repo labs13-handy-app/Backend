99d79d4590b79689fbb44998eed0c626f9301510
guillsav <guillsav@gmail.com>
2019-06-05 13:36:54 -0400

    Changes in database schema and new project_agreement table created

--- data/seeds/002_projects.js
+++ data/seeds/002_projects.js
@@ -6,35 +6,35 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('projects').insert([
         {
-          user_id: 2,
+          homeowner_id: 2,
           description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
           images: 'img',
           materials_included: 'yes'
         },
         {
-          user_id: 2,
+          homeowner_id: 2,
           description:
             'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. ',
           images: 'img',
           materials_included: 'yes'
         },
         {
-          user_id: 3,
+          homeowner_id: 3,
           description:
             'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
           images: 'img',
           materials_included: 'no'
         },
         {
-          user_id: 4,
+          homeowner_id: 4,
           description:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Cursus euismod quis viverra nibh cras pulvinar. Commodo viverra maecenas accumsan lacus vel facilisis. ',
           images: 'img',
           materials_included: 'yes'
         },
         {
-          user_id: 5,
+          homeowner_id: 5,
           description:
             'Ullamcorper dignissim cras tincidunt lobortis feugiat. A diam maecenas sed enim. Ut tellus elementum sagittis vitae et leo. Eu non diam phasellus vestibulum lorem sed risus ultricies. Leo vel fringilla est ullamcorper eget nulla facilisi.',
           images: 'img',
--- data/seeds/003_bids.js
+++ data/seeds/003_bids.js
@@ -7,36 +7,36 @@ exports.seed = function(knex, Promise) {
       return knex('bids').insert([
         {
           project_id: 2,
-          user_id: 3,
-          price: '15',
+          contractor_id: 3,
+          price: 15,
           time: '03',
           materials_included: 'no'
         },
         {
           project_id: 2,
-          user_id: 4,
-          price: '16',
+          contractor_id: 4,
+          price: 16,
           time: '02',
           materials_included: 'no'
         },
         {
           project_id: 5,
-          user_id: 1,
-          price: '10',
+          contractor_id: 1,
+          price: 10,
           time: '03',
           materials_included: 'yes'
         },
         {
           project_id: 5,
-          user_id: 2,
-          price: '17',
+          contractor_id: 2,
+          price: 17,
           time: '01',
           materials_included: 'no'
         },
         {
           project_id: 3,
-          user_id: 5,
-          price: '20',
+          contractor_id: 5,
+          price: 20,
           time: '05',
           materials_included: 'yes'
         }
