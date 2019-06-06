af22e75d8ab59b87086d31d9b8cce00428a8dcf2
guillsav <guillsav@gmail.com>
2019-06-06 16:53:44 -0400

    get users route added, restricted middleware modified to get user

--- config/restricted-middleware.js
+++ config/restricted-middleware.js
@@ -1,19 +1,26 @@
 const jwtDecode = require('jwt-decode');
 const jwt = require('jsonwebtoken');
+const db = require('../auth-route/register-model.js');
 
 const secrets = require('./secret.js');
 
-function restricted(req, res, next) {
+const restricted = async (req, res, next) => {
   const auth = req.headers.authorization;
   const userToken = auth.replace(/Bearer /g, '');
 
   if (!userToken) {
     res.status(401).json({message: 'You must be logged in to see that.'});
   } else {
-    const token = jwtDecode(userToken);
-    req.decodedJwt = token;
-    next();
+    const token = await jwtDecode(userToken);
+    const user = await db.getUserByName(token.nickname);
+    if (!user) {
+      res.status(404).json({errorMessage: `User doesn't exist!`});
+    } else {
+      req.user = user;
+      req.decodedJwt = token;
+      next();
+    }
   }
-}
+};
 
 module.exports = restricted;
--- data-routes/users.js
+++ data-routes/users.js
@@ -1,48 +1,62 @@
 const router = require('express').Router();
 const db = require('../data/dbConfig.js');
+const userDb = require('../auth-route/register-model.js');
+const jwChecks = require('../middleware/jwtChecks.js');
+const restricted = require('../config/restricted-middleware.js');
 
-router.get('/:id', (req,res)=>{
-    db('users')
-    .where({id:req.params.id})
+router.get('/', async (req, res) => {
+  try {
+    const users = await userDb.getUser();
+    res.status(200).json(users);
+  } catch (e) {
+    res
+      .status(500)
+      .json({errorMessage: `Server error couldn't retrieve users!`});
+  }
+});
+
+router.get('/:id', jwChecks, restricted, async (req, res) => {
+  db('users')
+    .where({id: req.user.id})
     .first()
-    .then(user=>{
-        if(user){
+    .then(user => {
+      if (user) {
         db('projects')
-        .where({homeowner_id:req.params.id})
-        .then(projects=>{
-          user.projects=projects;
-          res.status(200).json(user)
-        })
-      }else{
-          res.status(404).json({message:'The User with the Specified ID does not exist'})
+          .where({homeowner_id: req.params.id})
+          .then(projects => {
+            user.projects = projects;
+            res.status(200).json(user);
+          });
+      } else {
+        res
+          .status(404)
+          .json({message: 'The User with the Specified ID does not exist'});
       }
     })
-    .catch(err=>{
-        res.status(500).json({error:err,message:'Unable to find the Specified User at this time.'})
-    })
-})
+    .catch(err => {
+      res.status(500).json({
+        error: err,
+        message: 'Unable to find the Specified User at this time.'
+      });
+    });
+});
 
 router.put('/:id', (req, res) => {
-
-    db('users')
-
-    .where({id:req.params.id})
-
+  db('users')
+    .where({id: req.params.id})
     .update(req.body)
 
-    .then(count=>{
-
-      if (count>0) {
-        res.status(200).json({message:`${count} Project was updated`})
-      
-    }else{
-        res.status(404).json({message:'the specified Proect does not exist'})
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({message: `${count} Project was updated`});
+      } else {
+        res.status(404).json({message: 'the specified Proect does not exist'});
       }
     })
 
-    .catch(err =>{
-      res.status(500).json(err.message)
-    })
-  });
+    .catch(err => {
+      res.status(500).json(err.message);
+    });
+});
 
-module.exports=router
+module.exports = router;
