b3e3fa7b55f079e1016f17e632a3649e39bad75d
guillsav <guillsav@gmail.com>
2019-06-07 11:46:15 -0400

    New db migrations, seeds updated

--- auth-route/register.js
+++ auth-route/register.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const jwtDecode = require('jwt-decode');
 const db = require('./register-model.js');
-const secrets=require('../config/secret.js')
-const jwt =require('jsonwebtoken')
+// const secrets = require('../config/secret.js');
+const jwt = require('jsonwebtoken');
 
 router.post('/', async (req, res) => {
   try {
@@ -15,47 +15,45 @@ router.post('/', async (req, res) => {
 
       const user = {
         email: decode.name,
-        name: decode.nickname,
         nickname: decode.nickname
       };
+      console.log(user);
 
-      const foundUser = await db.getUserByName(user.name);
-      if (!foundUser) {
-        // if the user doesn't exist create a user object that reflect the database schema.
-        const newUser = {
-          name: user.name,
-          nickname: user.nickname,
-          email: user.email
-        };
+      const foundUser = await db.getUserByName(user.nickname);
 
-        const data = await db.addUser(newUser);
-        res.status(201).json(data);
+      if (foundUser) {
+        res.status(200).json({foundUser});
       } else {
-        const token= generateToken(foundUser)
-        res.status(200).json({foundUser,token});
+        // if the user doesn't exist create a user object that reflect the database schema.
+        // const newUser = {
+        //   nickname: user.nickname,
+        //   email: user.email
+        // };
+
+        const data = await db.addUser(user);
+        res.status(201).json(user);
       }
     } else {
       res.status(400).json({errorMessage: 'Invalid Credentials!'});
     }
   } catch (err) {
-    res.send(err.message).json({message: 'unable to sign up'});
+    res.send(err.message).json({message: 'Unable to sign up'});
   }
 });
 
-
-function generateToken(user){
-  const payload={
-      subject: user.id,
-      name:user.nickname,
-      account_type:user.account_type,
-      stripe_id:user.stripe_id,
-      payout_id:user.payout_id,
-      email:user.email
-  };
-  const options={
-      expiresIn:'1h'
-  };
-      return jwt.sign(payload, secrets.jwtSecret, options)
-  }
+// function generateToken(user) {
+//   const payload = {
+//     subject: user.id,
+//     name: user.nickname,
+//     account_type: user.account_type,
+//     stripe_id: user.stripe_id,
+//     payout_id: user.payout_id,
+//     email: user.email
+//   };
+//   const options = {
+//     expiresIn: '1h'
+//   };
+//   return jwt.sign(payload, secrets.jwtSecret, options);
+// }
 
 module.exports = router;
