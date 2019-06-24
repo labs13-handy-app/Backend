ba50501789db739b92409394ddcb164a469d1023
guillsav <guillsav@gmail.com>
2019-06-24 13:46:12 -0400

    .rows removed from map

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 // Update with your config settings.
-require('dotenv').config();
-const pg = require('pg');
-pg.defaults.ssl = true;
+// require('dotenv').config();
+// const pg = require('pg');
+// pg.defaults.ssl = true;
 
 module.exports = {
   development: {
